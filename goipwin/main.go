28551766221ad3e926f0a462d5fcbc2e57bc4c1c
Carles Mateo <carles.mateo@gmail.com>
2025-12-25 13:32:44 +0100

    Cada minut, quan canvia la IP, hauria de rebre el focus

--- goipwin/main.go
+++ goipwin/main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -34,18 +37,70 @@ func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("GoIP Public IP")
 
-	ipLabel := widget.NewLabel("Prem el botó per obtenir la IP...")
+	var lastIP string
+	var mu sync.Mutex
+
+	// Use binding to update label safely from any thread
+	ipBinding := binding.NewString()
+	ipBinding.Set("Prem el botó per obtenir la IP...")
+
+	ipLabel := widget.NewLabelWithData(ipBinding)
 	ipLabel.Alignment = fyne.TextAlignCenter
 
+	updateIP := func() {
+		ipBinding.Set("Obtenint IP...")
+		go func() {
+			ip, err := getPublicIP()
+			if err != nil {
+				ipBinding.Set(fmt.Sprintf("Error: %v", err))
+				return
+			}
+
+			mu.Lock()
+			currentLast := lastIP
+			lastIP = ip
+			mu.Unlock()
+
+			ipBinding.Set(fmt.Sprintf("La teva adreça IP pública és: %s", ip))
+			_ = currentLast
+		}()
+	}
+
 	refreshBtn := widget.NewButton("Refrescar IP", func() {
-		ipLabel.SetText("Obtenint IP...")
-		ip, err := getPublicIP()
-		if err != nil {
-			ipLabel.SetText(fmt.Sprintf("Error: %v", err))
-		} else {
-			ipLabel.SetText(fmt.Sprintf("La teva adreça IP pública és: %s", ip))
-		}
+		updateIP()
 	})
+
+	// Background Ticker
+	go func() {
+		ticker := time.NewTicker(1 * time.Minute)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			ip, err := getPublicIP()
+			if err != nil {
+				continue
+			}
+
+			mu.Lock()
+			oldIP := lastIP
+			if ip != lastIP {
+				lastIP = ip
+			}
+			mu.Unlock()
+
+			if oldIP != "" && oldIP != ip {
+				ipBinding.Set(fmt.Sprintf("La teva adreça IP pública és: %s", ip))
+				// Note: Window focus requires UI thread.
+				// Since direct access caused a crash and Driver().RunOnUIThread was reported undefined,
+				// we are currently only updating the text which is thread-safe via binding.
+				// To implement focus, we would need a valid way to dispatch to main thread.
+				// myWindow.Show()
+				// myWindow.RequestFocus()
+			} else if oldIP == "" {
+				ipBinding.Set(fmt.Sprintf("La teva adreça IP pública és: %s", ip))
+			}
+		}
+	}()
 
 	myWindow.SetContent(container.NewVBox(
 		widget.NewLabelWithStyle("Consultor d'IP Pública", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
